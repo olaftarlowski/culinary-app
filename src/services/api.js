cff6487a8539a35f35493d055592c7996163ef86
OlfiDell <bobr.rozrabiaka123@gmail.com>
2022-09-09 14:05:43 +0200

    adjusted to proxy

--- src/services/api.js
+++ src/services/api.js
@@ -1,13 +1,15 @@
 import axios from "axios";
 
-const API_KEY = `Bearer ${process.env.REACT_APP_AIRTABLE_API_KEY}`;
-const API_URL = process.env.REACT_APP_AIRTABLE_API_URL;
+// const API_KEY = `Bearer ${process.env.REACT_APP_AIRTABLE_API_KEY}`;
+// const API_URL = process.env.REACT_APP_AIRTABLE_API_URL;
+
+const API_URL = "http://localhost:3001/api";
 
 const getData = async () => {
-  axios.defaults.headers = {
-    "Content-Type": "application/json",
-    Authorization: API_KEY,
-  };
+  // axios.defaults.headers = {
+  //   "Content-Type": "application/json",
+  //   Authorization: API_KEY,
+  // };
 
   const res = await axios.get(`${API_URL}/recipes`).catch((error) => {
     console.log(error);
